076e33164be8f7cab7c6e81091b97d336d4827d2
unknown <njorogekamau63@gmail.com>
2023-05-27 19:49:54 +0300

    a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.

--- script.js
+++ script.js
@@ -4,23 +4,58 @@ function getComputerChoice() {
     return choices[randomIndex];
   }
 
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection) {
+playerSelection = playerSelection.toLowerCase();
+
+const winningConditions = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+};
+
+if (playerSelection === computerSelection) {
+    return "It's a tie!";
+}
+
+if (winningConditions[playerSelection] === computerSelection) {
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+}
+
+return "You Lose! " + computerSelection + " beats " + playerSelection;
+}
   
-    const winningConditions = {
-      rock: "scissors",
-      paper: "rock",
-      scissors: "paper",
-    };
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
   
-    if (playerSelection === computerSelection) {
-      return "It's a tie!";
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt("Enter your choice (Rock/Paper/Scissors):");
+      const computerSelection = getComputerChoice();
+      const result = playRound(playerSelection, computerSelection);
+  
+      console.log("Round " + round + ": " + result);
+  
+      if (result.includes("Win")) {
+        playerScore++;
+      } else if (result.includes("Lose")) {
+        computerScore++;
+      }
     }
   
-    if (winningConditions[playerSelection] === computerSelection) {
-      return "You Win! " + playerSelection + " beats " + computerSelection;
-    }
+    console.log("Game Over!");
   
-    return "You Lose! " + computerSelection + " beats " + playerSelection;
-  }
+    if (playerScore > computerScore) {
+      console.log("You won the game!");
+    } else if (playerScore < computerScore) {
+      console.log("You lost the game!");
+    } else {
+      console.log("It's a tie! No one wins.");
+    }
+}
   
